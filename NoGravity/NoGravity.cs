2909642e29b772d431ab6038b10a0de68b15c6a6
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-04-28 13:41:43 +0100

    First commit
    I need to tidy some stuff up later with the git

---
+++ NoGravity/NoGravity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+namespace NoGravity
+{
+    public class Load
+    {
+        public static void Init()
+        {
+            new GameObject("No Gravity", typeof(NoGravity));
+        }
+    }
+
+    public class NoGravity : MonoBehaviour
+    {
+        private bool isDisabled;
+
+        public static void Init()
+        {
+            new GameObject("No Gravity", typeof(NoGravity));
+        }
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        private void OnGUI()
+        {
+            if (isDisabled)
+            {
+                if (GUI.Button(new Rect(10, 10, 150, 100), "Enable Gravity"))
+                {
+                    Physics.gravity = new Vector3(0, -9.3f, 0);
+                    isDisabled = false;
+                }
+            }
+            else
+            {
+                if (GUI.Button(new Rect(10, 10, 150, 100), "Disable Gravity"))
+                {
+                    Physics.gravity = new Vector3(0, 0, 0);
+                    isDisabled = true;
+                }
+            }
+
+        }
+    }
+
+}
---
+++ VTOLVR-ModLoader/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VTOLVR_ModLoader
+{
+    public partial class MainWindow : Window
+    {
+        private static string modsFolder = @"\mods";
+        private static string injector = @"\MonoInjector64.exe";
+        private string root;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start()
+        {
+            root = Directory.GetCurrentDirectory();
+            CheckFolder();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Start();
+            Task task = LaunchGame();
+        }
+
+        private void CheckFolder()
+        {
+            if (!Directory.Exists(root + modsFolder))
+            {
+                Directory.CreateDirectory(root + modsFolder);
+            }
+        }
+        private async Task LaunchGame()
+        {
+            Process[] processesByName = Process.GetProcessesByName("VTOLVR");
+            if (processesByName.Length != 0)
+            {
+                LoadMods();
+            }
+            else
+            {
+                Process.Start("steam://run/667970");
+                int count = 0;
+                while (Process.GetProcessesByName("VTOLVR").Length == 0)
+                {
+                    count++;
+                    await Task.Delay(1000);
+                    if (count >= 10)
+                    {
+                        Process.GetCurrentProcess().Kill();
+                        return;
+                    }
+                }
+                await Task.Delay(40000);
+                LoadMods();
+            }
+        }
+
+        private void LoadMods()
+        {
+            DirectoryInfo folder = new DirectoryInfo(root + modsFolder);
+            foreach (FileInfo file in folder.GetFiles("*.dll"))
+            {
+                try
+                {
+                    string start = string.Format(" -t {0} -d {1} -n {2} -c {3} -m {4}", "VTOLVR.exe", @"mods\" + file.Name, file.Name.ToString().Split('.')[0], "Load", "Init");
+                    Process.Start(root + injector, start);
+                }
+                catch
+                {
+                }
+            }
+
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void InjectButton(object sender, RoutedEventArgs e)
+        {
+            Start();
+            LoadMods();
+        }
+    }
+}
