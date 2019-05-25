6d059f31f506e9f795814cf9f199139e6eed7965
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-05-25 10:35:33 +0100

    Added a Debug.log to the no gravity mod

--- Example Mods/NoGravity/NoGravity.cs
+++ Example Mods/NoGravity/NoGravity.cs
@@ -35,6 +35,7 @@ namespace NoGravity
                 if (GUI.Button(new Rect(10, 10, 150, 100), "Enable Gravity"))
                 {
                     Physics.gravity = new Vector3(0, -9.3f, 0);
+                    Debug.Log("Gravity has been enabled");
                     isDisabled = false;
                 }
             }
@@ -43,6 +44,7 @@ namespace NoGravity
                 if (GUI.Button(new Rect(10, 10, 150, 100), "Disable Gravity"))
                 {
                     Physics.gravity = new Vector3(0, 0, 0);
+                    Debug.Log("Gravity has been disabled");
                     isDisabled = true;
                 }
             }
