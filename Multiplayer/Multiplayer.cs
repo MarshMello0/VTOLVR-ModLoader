8200a93ead78ccd5d2c438a98d64258a8bc01724
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-05-25 19:37:53 +0100

    Multiplayer first commit

---
+++ Multiplayer/Multiplayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Multiplayer
+{
+    public class Load
+    {
+        public static void Init()
+        {
+            new GameObject("Multiplayer", typeof(Multiplayer));
+        }
+    }
+    public class Multiplayer : MonoBehaviour
+    {
+
+    }
+}
