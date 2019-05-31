cc2756060eb40948b7ed8573b0e3bc1093e2bd84
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-05-31 13:38:29 +0100

    Client Mod Created

---
+++ Multiplayer/Multiplayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Multiplayer
+{ 
+    public class Load
+    {
+        public static void Init()
+        {
+            new GameObject("Multiplayer", typeof(Multiplayer));
+        }
+    }
+    public class Multiplayer : MonoBehaviour
+    {
+
+    }
+}
