c077581c4dc824acad7aaabf5f77fe27f3c23e21
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-05-31 13:21:25 +0100

    Server Files & Plugin Created

---
+++ VTOLServerPlugin/VTOLServerPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DarkRift.Server;
+
+public class VTOLServerPlugin : Plugin
+{
+    public override bool ThreadSafe => false;
+
+    public override Version Version => new Version(0, 0, 1);
+
+    public VTOLServerPlugin(PluginLoadData pluginLoadData) : base(pluginLoadData)
+    {
+
+    }
+}
