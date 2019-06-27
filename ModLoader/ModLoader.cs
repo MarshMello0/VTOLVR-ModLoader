7da13bf65ada5ea515c1bcd639d54f0a5ade7269
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-06-27 16:16:08 +0100

    Default Mod Base

---
+++ ModLoader/ModLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ModLoader
+{
+    public class Load
+    {
+        public static void Init()
+        {
+            new GameObject("Mod Loader", typeof(ModLoader));
+        }
+    }
+
+    public class ModLoader : MonoBehaviour
+    {
+        public static ModLoader _instance;
+
+        private void Awake()
+        {
+            //This is to make sure we only have one of these in the game
+            if (!_instance)
+            {
+                _instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+    }
+}
