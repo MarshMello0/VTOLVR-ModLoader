9840b174d74445f8c311a14ca5cf27df1b4a8f74
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-06-27 17:22:10 +0100

    Player Log message in Mod Loader Mod

--- ModLoader/ModLoader.cs
+++ ModLoader/ModLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.CrashReportHandler;
 
 namespace ModLoader
 {
@@ -25,12 +26,48 @@ namespace ModLoader
             {
                 _instance = this;
                 DontDestroyOnLoad(this.gameObject);
+                PlayerLogText();
             }
             else
             {
                 Destroy(this.gameObject);
                 return;
             }
+        }
+
+        private void PlayerLogText()
+        {
+            string playerLogMessage = @" 
+                                                                                                         
+                                                                                                         
+ #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  ##### 
+                                                                                                         
+ #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  ##### 
+                                                                                                         
+ #     #                                              #     #                                            
+ ##   ##   ####   #####   #####   ######  #####       #     #  ######  #####    ####   #   ####   #    # 
+ # # # #  #    #  #    #  #    #  #       #    #      #     #  #       #    #  #       #  #    #  ##   # 
+ #  #  #  #    #  #    #  #    #  #####   #    #      #     #  #####   #    #   ####   #  #    #  # #  # 
+ #     #  #    #  #    #  #    #  #       #    #       #   #   #       #####        #  #  #    #  #  # # 
+ #     #  #    #  #    #  #    #  #       #    #        # #    #       #   #   #    #  #  #    #  #   ## 
+ #     #   ####   #####   #####   ######  #####          #     ######  #    #   ####   #   ####   #    # 
+
+Thank you for download VTOL VR Mod loader by . Marsh.Mello .
+
+Please don't report bugs unless you can reproduce them without any mods loaded
+if you are having any issues with mods and would like to report a bug, please contact @. Marsh.Mello .#3194 
+on the offical VTOL VR Discord or post an issue on github. 
+
+VTOL VR Discord Server: https://discord.gg/azNkZHj
+Mod Loader Github: https://github.com/MarshMello0/VTOLVR-ModLoader
+
+Special Thanks to Ketkev and Nebriv with help in testing and modding.
+
+ #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  ##### 
+                                                                                                         
+ #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  #####  ##### 
+";
+            Debug.Log(playerLogMessage);
         }
     }
 }
